fa08edd5cc3fecb1e0316a73f9bebe7984323af5
Jeremy <pawjwp@users.noreply.github.com>
2025-08-25 15:42:26 -0400

    Misc temperature/thirst balancing
    Added string armor to ColdSweat insulation items
    Disabled temperature acclimation
    Reduced desert temperature to 105
    Added purified water rewards to starting quests
    Changed dirty water from dirt recipes to use purity
    Increased dirt costs for dirty water bucket

--- kubejs/server_scripts/crafting/shapeless/dirty_water_from_dirt.js
+++ kubejs/server_scripts/crafting/shapeless/dirty_water_from_dirt.js
@@ -1,14 +1,17 @@
 ServerEvents.recipes(event => {
     event.shapeless(
-        Item.of('minecraft:potion', '{Potion:"minecraft:water"}'), [
+        Item.of('minecraft:potion', '{Potion:"minecraft:water",Purity:0}'), [
             'minecraft:glass_bottle',
             '#minecraft:dirt'
         ]
     )
 	
     event.shapeless(
-        Item.of('minecraft:water_bucket'), [
+        Item.of('minecraft:water_bucket', '{Purity:0}'), [
             'minecraft:bucket',
+            '#minecraft:dirt',
+            '#minecraft:dirt',
+            '#minecraft:dirt',
             '#minecraft:dirt'
         ]
     )
