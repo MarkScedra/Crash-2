58c9ed6c02715c5652f8270bf1885159d49b6329
Jeremy <pawjwp@users.noreply.github.com>
2025-08-25 15:31:06 -0400

    Crucible water recipe changes & centrifuge tweaks
    Reworked crucible water melting recipes, combining them into one file
    Rebalanced centrifuge recipes to match
    Lowered centrifuge water production energy consumption

--- kubejs/server_scripts/thermal/centrifuge.js
+++ kubejs/server_scripts/thermal/centrifuge.js
@@ -3,37 +3,37 @@ ServerEvents.recipes(event => {
     event.recipes.thermal.centrifuge([
         Item.of('minecraft:stick').withChance(0.5),
         Item.of('mekanism:bio_fuel').withChance(0.1),
-        Fluid.of('minecraft:water', 50,  {Purity: 2})
-    ], '#minecraft:saplings')
+        Fluid.of('minecraft:water', 50, {Purity: 2})
+    ], '#minecraft:saplings').energy(1000)
 
     event.recipes.thermal.centrifuge([
         Item.of('minecraft:stick').withChance(0.1),
         Item.of('mekanism:bio_fuel').withChance(0.1),
-        Fluid.of('minecraft:water', 50,  {Purity: 2})
-    ], '#minecraft:leaves')
+        Fluid.of('minecraft:water', 50, {Purity: 2})
+    ], '#minecraft:leaves').energy(1000)
 
     event.recipes.thermal.centrifuge([
-        Item.of('mekanism:bio_fuel').withChance(0.3),
-        Fluid.of('minecraft:water', 125,  {Purity: 2})
-    ], '#forge:fruits')
+        Item.of('mekanism:bio_fuel').withChance(0.15),
+        Fluid.of('minecraft:water', 50, {Purity: 2})
+    ], '#minecraft:flowers').energy(1000)
 
     event.recipes.thermal.centrifuge([
-        Item.of('mekanism:bio_fuel').withChance(0.2),
-        Fluid.of('minecraft:water', 50,  {Purity: 2})
-    ], '#minecraft:flowers')
+        Item.of('mekanism:bio_fuel').withChance(0.25),
+        Fluid.of('minecraft:water', 50, {Purity: 2})
+    ], '#forge:crops').energy(1000)
 
     event.recipes.thermal.centrifuge([
-        Item.of('mekanism:bio_fuel').withChance(0.3),
-        Fluid.of('minecraft:water', 50,  {Purity: 2})
-    ], '#forge:crops')
+        Item.of('mekanism:bio_fuel').withChance(0.25),
+        Fluid.of('minecraft:water', 100, {Purity: 2})
+    ], '#forge:fruits').energy(1000)
 
     event.recipes.thermal.centrifuge([
-        Item.of('mekanism:bio_fuel').withChance(0.3),
-        Fluid.of('minecraft:water', 125,  {Purity: 2})
-    ], 'minecraft:cactus')
+        Item.of('mekanism:bio_fuel').withChance(0.25),
+        Fluid.of('minecraft:water', 125, {Purity: 2})
+    ], 'minecraft:cactus').energy(2000)
 
     event.recipes.thermal.centrifuge([
         Item.of('minecraft:sand').withChance(0.25),
-        Fluid.of('minecraft:water', 250,  {Purity: 0})
-    ], 'minecraft:cactus')
+        Fluid.of('minecraft:water', 250, {Purity: 0})
+    ], 'minecraft:cactus').energy(2000)
 })
