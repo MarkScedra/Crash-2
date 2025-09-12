b0c8659be0fb3bbb5b89766b95d30744fb462d3a
Jeremy <pawjwp@users.noreply.github.com>
2025-09-12 15:20:34 -0400

    Added Appleskin back, CRF tweaks
    Added Appleskin mod
    Added configs for Appleskin
    Disabled quark food tooltips
    Hid some cutting board recipes
    Fixed typo in freezing recipe

---
+++ kubejs/client_scripts/refurbished_furniture_removal_jei.js
@@ -0,0 +1,4 @@
+JEIEvents.removeRecipes(event => {
+    event.remove({ type: "refurbished_furniture:cutting_board_combining" })
+    event.remove({ type: "refurbished_furniture:cutting_board_slicing" })
+})
