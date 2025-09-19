09df710c418430b4260886784d64ed963dbd175e
Jeremy <pawjwp@users.noreply.github.com>
2025-09-19 12:49:23 -0400

    Misc changes
    Added stillsuit armor tier (actual items are not implemented yet, just the tier with stats)
    Decreased string armor durability
    Corrected mod list in JEI sort order
    Added a kubejs test item that currently provides thirst when right-clicked

---
+++ kubejs/startup_scripts/custom_items/stillsuit.js
@@ -0,0 +1,26 @@
+ItemEvents.armorTierRegistry(event => {
+    event.add('stillsuit', tier => {
+        tier.durabilityMultiplier = 15 // Each slot will be multiplied with [13, 15, 16, 11]
+        tier.slotProtections = [2, 5, 6, 2] // Slot indicies are [FEET, LEGS, BODY, HEAD]
+        tier.enchantmentValue = 10
+        tier.equipSound = 'minecraft:item.armor.equip_generic'
+        tier.repairIngredient = 'thermal:cured_rubber'
+        tier.toughness = 1.0
+        tier.knockbackResistance = 0.0
+    })
+});
+/*
+StartupEvents.registry("item", (event) => {
+    event
+        .create("stillsuit_helmet", "helmet")
+        .tier("stillsuit")
+    event
+        .create("stillsuit_chestplate", "chestplate")
+        .tier("stillsuit")
+    event
+        .create("stillsuit_leggings", "leggings")
+        .tier("stillsuit")
+    event
+        .create("stillsuit_boots", "boots")
+        .tier("stillsuit")
+});*/
