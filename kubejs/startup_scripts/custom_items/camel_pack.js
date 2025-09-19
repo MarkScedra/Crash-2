3fc3b8fb3e2169728c05b609acb01074bc8cfa14
Jeremy <pawjwp@users.noreply.github.com>
2025-09-19 13:10:18 -0400

    Added model properties to camel pack
    Added camel pack model properties, allowing for custom models based on fluid type and amount

--- kubejs/startup_scripts/custom_items/camel_pack.js
+++ kubejs/startup_scripts/custom_items/camel_pack.js
@@ -66,3 +66,142 @@ StartupEvents.registry('item', (event) => {
                 })
         )
 })
+
+
+ItemEvents.modelProperties((e) => {
+    const capacity = 4000
+
+    e.register('kubejs:camel_pack',
+        new ResourceLocation('stored'),
+        /** @type {Internal.ClampedItemPropertyFunction} */
+        (stack) => {
+            // read fluid NBT
+            if (!stack || !stack.nbt || !stack.nbt.contains('Fluid')) return 0
+            var fluidTag = stack.nbt.get('Fluid')
+            if (!fluidTag || !fluidTag.contains) return 0
+
+            var amount = fluidTag.contains('Amount') ? fluidTag.getInt('Amount') : 0
+
+            return Math.max(0, Math.min(1, amount / capacity))
+
+        }
+    )
+
+    e.register('kubejs:camel_pack',
+        new ResourceLocation('fluid'),
+        /** @type {Internal.ClampedItemPropertyFunction} */
+        (stack) => {
+            // read fluid NBT
+            if (!stack || !stack.nbt || !stack.nbt.contains('Fluid')) return 0
+            var fluidTag = stack.nbt.get('Fluid')
+            if (!fluidTag || !fluidTag.contains) return 0
+
+            var fluidType = null
+            if (fluidTag.contains('FluidName')) fluidType = fluidTag.getString('FluidName')
+
+            if (!fluidType) return 0
+
+            // switch color based on fluid type
+            switch (fluidType) {
+                // 0.01: blue
+                case 'minecraft:water': return 0.01 // functions
+                case 'mekanism:heavy_water': return 0.01
+
+                // 0.02: orange
+                case 'minecraft:lava': return 0.02 // functions
+                case 'thermal:syrup': return 0.02
+                case 'thermal:sap': return 0.02
+                case 'thermal:light_oil': return 0.02
+                case 'tconstruct:magma': return 0.02
+                case 'tconstruct:ichor': return 0.02
+                case 'tconstruct:blazing_blood': return 0.02
+                case 'pneumaticcraft:diesel': return 0.02
+                case 'create:tea': return 0.02
+
+                // 0.03: white
+                case 'minecraft:milk': return 0.03 // functions
+                case 'thermal:latex': return 0.03
+                case 'tconstruct:venom': return 0.03
+                case 'tconstruct:powdered_snow': return 0.03
+                case 'pneumaticcraft:yeast_culture': return 0.03
+                case 'pneumaticcraft:plastic': return 0.03
+                case 'pneumaticcraft:ethanol': return 0.03
+                case 'industrialforegoing:latex': return 0.03
+                case 'industrialforegoing:ether_gas': return 0.03
+                case 'biggerreactors:steam': return 0.03
+                case 'ad_astra:oxygen': return 0.03
+
+                // 0.04: experience
+                case 'tconstruct:earth_slime': return 0.04
+                case 'sophisticatedcore:xp_still': return 0.04
+                case 'pneumaticcraft:memory_essence': return 0.04
+                case 'pneumaticcraft:etching_acid': return 0.04
+                case 'mob_grinding_utils:fluid_xp': return 0.04
+                case 'industrialforegoing:essence': return 0.04
+                case 'enderio:xp_juice': return 0.04
+                case 'cofh_core:experience': return 0.04
+                case 'biggerreactors:liquid_uranium': return 0.04
+
+                // 0.05: purple
+                case 'tconstruct:ender_slime': return 0.05
+                case 'industrialforegoing:sludge': return 0.05
+                case 'industrialforegoing:pink_slime': return 0.05
+                case 'industrialforegoing:biofuel': return 0.05
+                case 'exdeorum:witch_water': return 0.05
+
+                // 0.06: brown
+                case 'thermal:tree_oil': return 0.06
+                case 'tconstruct:rabbit_stew': return 0.06
+                case 'tconstruct:mushroom_stew': return 0.06
+                case 'tconstruct:meat_soup': return 0.06
+                case 'tconstruct:liquid_soul': return 0.06
+                case 'industrialforegoing:sewage': return 0.06
+                case 'industrialforegoing:meat': return 0.06
+                case 'create:chocolate': return 0.06
+
+                // 0.07: yellow
+                case 'thermal:resin': return 0.07
+                case 'thermal:refined_fuel': return 0.07
+                case 'thermal:glowstone': return 0.07
+                case 'tconstruct:honey': return 0.07
+                case 'pneumaticcraft:vegetable_oil': return 0.07
+                case 'pneumaticcraft:lubricant': return 0.07
+                case 'pneumaticcraft:lpg': return 0.07
+                case 'pneumaticcraft:gasoline': return 0.07
+                case 'pneumaticcraft:biodiesel': return 0.07
+                case 'mekanism:chlorine': return 0.07
+                case 'mekanism:brine': return 0.07
+                case 'createaddition:bioethanol': return 0.07
+                case 'create:honey': return 0.07
+                case 'cofh_core:honey': return 0.07
+
+                // 0.08: red
+                case 'thermal:redstone': return 0.08
+                case 'thermal:heavy_oil': return 0.08
+                case 'tconstruct:beetroot_soup': return 0.08
+                case 'ad_astra:fuel': return 0.08
+
+                // 0.09: teal
+                case 'thermal:ender': return 0.09
+                case 'tconstruct:molten_ender': return 0.09
+
+                // 0.10: cyan
+                case 'tconstruct:sky_slime': return 0.10
+                case 'pneumaticcraft:kerosene': return 0.10
+                case 'ad_astra:hydrogen': return 0.10
+                case 'ad_astra:cryo_fuel': return 0.10
+
+                // 0.00: black/gray (default)
+                case 'thermal:crude_oil': return 0.00
+                case 'thermal:creosote': return 0.00
+                case 'tconstruct:seared_stone': return 0.00
+                case 'tconstruct:scorched_stone': return 0.00
+                case 'pneumaticcraft:oil': return 0.00
+                case 'biggerreactors:liquid_obsidian': return 0.00
+                case 'ad_astra:oil': return 0.00
+
+                default: return 0.00
+            }
+        }
+    )
+})
