1c1fc527ae6b56f9cecd5a83dfb83cf5ce0e6cd0
Jeremy <pawjwp@users.noreply.github.com>
2025-10-18 21:51:34 -0400

    Added leather and wool insulation for curios slot
    Added leather and wool insulation for curios slot
    Added leather insulation recipe

--- kubejs/server_scripts/insulation_items.js
+++ kubejs/server_scripts/insulation_items.js
@@ -5,4 +5,16 @@ ColdSweatEvents.registries(event => {
             .insulation(50, 50)
             //.adaptiveInsulation(20, 0.1)
     })
+    
+    event.addInsulator(insulator => {
+        insulator.items('kubejs:leather_insulation')
+            .slot('curio')
+            .insulation(4, 4)
+    })
+    
+    event.addInsulator(insulator => {
+        insulator.items('kubejs:wool_insulation')
+            .slot('curio')
+            .insulation(12, 0)
+    })
 })
