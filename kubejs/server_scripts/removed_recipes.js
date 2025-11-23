3e12ababe72834bfe434923360713ef7b8d2bb00
Jeremy <pawjwp@users.noreply.github.com>
2025-11-22 22:45:38 -0500

    Removed duplicate CGM workbench recipe

--- kubejs/server_scripts/removed_recipes.js
+++ kubejs/server_scripts/removed_recipes.js
@@ -1,6 +1,7 @@
 ServerEvents.recipes(event => {
     // Removal by ID
     [
+        'cgm:workbench',
         'create:splashing/ice',
         'enderio:iron_gear',
         'industrialforegoing:diamond_gear',
