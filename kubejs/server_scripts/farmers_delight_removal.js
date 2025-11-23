030c1cacbd8eb1be9deea2d68f5f9e5b7b82154f
Jeremy <pawjwp@users.noreply.github.com>
2025-11-23 14:25:22 -0500

    Split meat tags and stages by animal type
    Split meat_sourced tag and meat stage into separate tags/stages for each meat type (beef, chicken, mutton, pork, rabbit, and any_animal)

--- kubejs/server_scripts/farmers_delight_removal.js
+++ kubejs/server_scripts/farmers_delight_removal.js
@@ -221,6 +221,12 @@ ServerEvents.tags('item', event => {
         'legumedelight:baked_beans'
     )
 
+
+
+    // AStages removal tags
+
+    // Animal products
+
     // Removed until player unlocks chickens
     event.add('desolate_planet:egg_sourced',
         'minecraft:cake',
@@ -276,91 +282,76 @@ ServerEvents.tags('item', event => {
         'farmersdelight:honey_glazed_ham'
     )
 
-    // Removed until player unlocks spawn eggs
-    event.add('desolate_planet:meat_sourced',
+
+
+    // Meats
+
+    // Removed until player unlocks cows
+    event.add('desolate_planet:beef_sourced',
         '#forge:cooked_beef',
-        '#forge:cooked_chicken',
-        '#forge:cooked_mutton',
-        '#forge:cooked_pork',
-        '#forge:foods/meat/cooked',
-        '#forge:foods/meat/raw',
         '#forge:raw_beef',
-        '#forge:raw_chicken',
-        '#forge:raw_mutton',
-        '#forge:raw_pork',
-        // 'culturaldelights:beef_burrito',
-        // 'culturaldelights:chicken_roll_slice',
-        // 'culturaldelights:chicken_roll',
-        // 'culturaldelights:chicken_taco',
-        // 'culturaldelights:egg_roll',
-        // 'culturaldelights:exotic_roll_medley',
-        // 'culturaldelights:mutton_sandwich',
-        // 'culturaldelights:pork_wrap',
-        // 'culturaldelights:spicy_curry',
-        'farmersdelight:bacon_and_eggs',
-        'farmersdelight:bacon_sandwich',
-        'farmersdelight:barbecue_stick',
         'farmersdelight:beef_stew',
+        'farmersdelight:hamburger',
+        'farmersdelight:pasta_with_meatballs',
+        'farmersdelight:steak_and_potatoes',
+        'farmersdelight:stuffed_potato',
+    )
+
+    // Removed until player unlocks chickens
+    event.add('desolate_planet:chicken_sourced',
+        '#forge:cooked_chicken',
+        '#forge:raw_chicken',
         'farmersdelight:chicken_sandwich',
         'farmersdelight:chicken_soup',
-        'farmersdelight:cod_roll',
-        'farmersdelight:dog_food',
-        'farmersdelight:fried_egg',
-        'farmersdelight:ham',
-        'farmersdelight:hamburger',
-        'farmersdelight:honey_glazed_ham_block',
-        'farmersdelight:honey_glazed_ham',
-        'farmersdelight:horse_feed',
-        'farmersdelight:mutton_chops',
-        'farmersdelight:mutton_wrap',
-        'farmersdelight:pasta_with_meatballs',
-        'farmersdelight:pasta_with_mutton_chop',
         'farmersdelight:roast_chicken',
         'farmersdelight:roast_chicken_block',
+    )
+
+    // Removed until player unlocks sheep
+    event.add('desolate_planet:mutton_sourced',
+        '#forge:cooked_mutton',
+        '#forge:raw_mutton',
+        'farmersdelight:mutton_chops',
+        'farmersdelight:mutton_wrap',
+        'farmersdelight:pasta_with_mutton_chop',
         'farmersdelight:roasted_mutton_chops_block',
         'farmersdelight:roasted_mutton_chops',
         'farmersdelight:shepherds_pie',
         'farmersdelight:shepherds_pie_block',
+    )
+
+    // Removed until player unlocks pigs
+    event.add('desolate_planet:pork_sourced',
+        '#forge:cooked_pork',
+        '#forge:raw_pork',
+        'farmersdelight:bacon_and_eggs',
+        'farmersdelight:bacon_sandwich',
+        'farmersdelight:ham',
+        'farmersdelight:honey_glazed_ham_block',
+        'farmersdelight:honey_glazed_ham',
         'farmersdelight:smoked_ham',
-        'farmersdelight:steak_and_potatoes',
-        'farmersdelight:stuffed_potato',
+    )
+
+    // Removed until player unlocks rabbits
+    event.add('desolate_planet:rabbit_sourced',
+        'minecraft:cooked_rabbit',
+        'minecraft:raw_rabbit',
         'minecraft:rabbit_stew',
-        // 'moredelight:chicken_salad',
-        // 'moredelight:chicken_sandwich_with_egg_and_tomato',
-        // 'moredelight:cooked_rice_with_beef',
-        // 'moredelight:cooked_rice_with_chicken_cuts',
-        // 'moredelight:cooked_rice_with_porkchop',
-        // 'moredelight:creamy_pasta_with_chicken_cuts',
-        // 'moredelight:creamy_pasta_with_ham',
-        // 'moredelight:diced_potatoes_with_beef',
-        // 'moredelight:diced_potatoes_with_chicken_cuts',
-        // 'moredelight:diced_potatoes_with_porkchop',
-        // 'moredelight:egg_sandwich',
-        // 'moredelight:egg_with_bacon_sandwich',
-        // 'moredelight:hamburger_with_egg',
-        // 'moredelight:loaded_hamburger',
-        // 'moredelight:omelette',
-        // 'moredelight:porkchop_sandwich',
-        // 'moredelight:simple_hamburger',
-        // 'moredelight:steak_sandwich',
-        // 'moredelight:toast_with_egg',
+    )
+
+    // Removed until player unlocks any spawn egg
+    event.add('desolate_planet:any_animal_sourced',
+        'farmersdelight:barbecue_stick',
+        'farmersdelight:dog_food',
+        'farmersdelight:horse_feed'
     )
 
     // Removed because fish don't exist in the desert
     event.add('desolate_planet:fish_sourced',
         '#forge:cooked_fishes',
         '#forge:raw_fishes',
-        // 'culturaldelights:cooked_calamari',
-        // 'culturaldelights:cooked_squid',
-        // 'culturaldelights:exotic_roll_medley',
-        // 'culturaldelights:fish_taco',
-        // 'culturaldelights:glow_squid',
-        // 'culturaldelights:pufferfish_roll',
-        // 'culturaldelights:raw_calamari',
-        // 'culturaldelights:rice_ball',
-        // 'culturaldelights:squid',
-        // 'culturaldelights:tropical_roll',
         'farmersdelight:baked_cod_stew',
+        'farmersdelight:cod_roll',
         'farmersdelight:fish_stew',
         'farmersdelight:squid_ink_pasta',
         'farmersdelight:rice_roll_medley_block',
@@ -368,6 +359,10 @@ ServerEvents.tags('item', event => {
         'farmersdelight:salmon_roll',
         'farmersdelight:grilled_salmon',
     )
+
+
+
+    // Environmental
 
     // Removed because kelp doesn't exist in the desert
     event.add('desolate_planet:kelp_sourced',
@@ -433,7 +428,72 @@ AStages.addRestrictionForTag('desolate_planet:honey_sourced', 'honey', 'desolate
     .setCanItemBeRightClicked(true)
     .setCanBeStoredInContainers(true)
 
-AStages.addRestrictionForTag('desolate_planet:meat_sourced', 'meat', 'desolate_planet:meat_sourced')
+AStages.addRestrictionForTag('desolate_planet:beef_sourced', 'beef', 'desolate_planet:beef_sourced')
+    // hides from JEI but is not restricted in any other way
+    .setRenderItemName(true)
+    .setHideTooltip(false)
+    .setCanPickedUp(true)
+    .setCanBeEquipped(true)
+    .setCanBeStoredInInventory(true)
+    .setCanAttack(true)
+    .setCanBePlaced(true)
+    .setCanItemBeLeftClicked(true)
+    .setCanItemBeRightClicked(true)
+    .setCanBeStoredInContainers(true)
+
+AStages.addRestrictionForTag('desolate_planet:chicken_sourced', 'chicken', 'desolate_planet:chicken_sourced')
+    // hides from JEI but is not restricted in any other way
+    .setRenderItemName(true)
+    .setHideTooltip(false)
+    .setCanPickedUp(true)
+    .setCanBeEquipped(true)
+    .setCanBeStoredInInventory(true)
+    .setCanAttack(true)
+    .setCanBePlaced(true)
+    .setCanItemBeLeftClicked(true)
+    .setCanItemBeRightClicked(true)
+    .setCanBeStoredInContainers(true)
+
+AStages.addRestrictionForTag('desolate_planet:mutton_sourced', 'mutton', 'desolate_planet:mutton_sourced')
+    // hides from JEI but is not restricted in any other way
+    .setRenderItemName(true)
+    .setHideTooltip(false)
+    .setCanPickedUp(true)
+    .setCanBeEquipped(true)
+    .setCanBeStoredInInventory(true)
+    .setCanAttack(true)
+    .setCanBePlaced(true)
+    .setCanItemBeLeftClicked(true)
+    .setCanItemBeRightClicked(true)
+    .setCanBeStoredInContainers(true)
+
+AStages.addRestrictionForTag('desolate_planet:pork_sourced', 'pork', 'desolate_planet:pork_sourced')
+    // hides from JEI but is not restricted in any other way
+    .setRenderItemName(true)
+    .setHideTooltip(false)
+    .setCanPickedUp(true)
+    .setCanBeEquipped(true)
+    .setCanBeStoredInInventory(true)
+    .setCanAttack(true)
+    .setCanBePlaced(true)
+    .setCanItemBeLeftClicked(true)
+    .setCanItemBeRightClicked(true)
+    .setCanBeStoredInContainers(true)
+
+AStages.addRestrictionForTag('desolate_planet:rabbit_sourced', 'rabbit', 'desolate_planet:rabbit_sourced')
+    // hides from JEI but is not restricted in any other way
+    .setRenderItemName(true)
+    .setHideTooltip(false)
+    .setCanPickedUp(true)
+    .setCanBeEquipped(true)
+    .setCanBeStoredInInventory(true)
+    .setCanAttack(true)
+    .setCanBePlaced(true)
+    .setCanItemBeLeftClicked(true)
+    .setCanItemBeRightClicked(true)
+    .setCanBeStoredInContainers(true)
+
+AStages.addRestrictionForTag('desolate_planet:any_animal_sourced', 'any_animal', 'desolate_planet:any_animal_sourced')
     // hides from JEI but is not restricted in any other way
     .setRenderItemName(true)
     .setHideTooltip(false)
