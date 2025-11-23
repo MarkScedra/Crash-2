aa1a86ed33eceefeb82f0ca6e2697e0ee0ec8073
Jeremy <pawjwp@users.noreply.github.com>
2025-11-23 01:17:28 -0500

    Updated compression dynamo fuel list
    Added biofuel
    Slightly reduced other fuel amounts

--- kubejs/server_scripts/thermal/fuel/diesel_compression.js
+++ kubejs/server_scripts/thermal/fuel/diesel_compression.js
@@ -1,15 +1,8 @@
 ServerEvents.recipes(event => {
-    event.recipes.thermal.compression_fuel('pneumaticcraft:diesel').energy(256000)
-})
-
-ServerEvents.recipes(event => {
-    event.recipes.thermal.compression_fuel('pneumaticcraft:kerosene').energy(320000)
-})
-
-ServerEvents.recipes(event => {
-    event.recipes.thermal.compression_fuel('pneumaticcraft:gasoline').energy(400000)
-})
-
-ServerEvents.recipes(event => {
-    event.recipes.thermal.compression_fuel('pneumaticcraft:lpg').energy(500000)
+    event.recipes.thermal.compression_fuel('createaddition:bioethanol').energy(100000);
+    event.recipes.thermal.compression_fuel('industrialforegoing:biofuel').energy(100000);
+    event.recipes.thermal.compression_fuel('pneumaticcraft:diesel').energy(200000);
+    event.recipes.thermal.compression_fuel('pneumaticcraft:kerosene').energy(300000);
+    event.recipes.thermal.compression_fuel('pneumaticcraft:gasoline').energy(400000);
+    event.recipes.thermal.compression_fuel('pneumaticcraft:lpg').energy(500000);
 })
